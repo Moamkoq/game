20e7b6a28dd7ab0642b459b4fb4d2c8a81174e9d
Moamkoq <149740022+Moamkoq@users.noreply.github.com>
2024-02-27 09:55:57 +0900

    Add files via upload

---
+++ game.js
@@ -0,0 +1,43 @@
+function startGame(){
+    gameLoop();
+}
+
+var count = 0;
+var personVis = false;
+var score = 0;
+
+
+function gameLoop(){
+    personVis = ! personVis
+
+    if (personVis == true){
+        var classToset = "character visible";
+    }
+    else{
+        var classToset = "character hidden";
+    }
+
+    var gamezone = document.getElementById("gamezone");
+
+    for ( i = 0; i<8; i++){
+        gamezone.children[i].className = classToset;
+        gamezone.children[i].onclick = function(){score -=2;}
+    }
+
+    var randomNum = Math.floor(Math.random()*8) +1;
+
+    gamezone.children[randomNum-1].className = classToset + " elprimo";
+    gamezone.children[randomNum-1].onclick = function(){score +=1;}
+   
+    count +=1;
+
+    if (count < 12){
+        // setTimeout(gameLoop, 3000);
+        setTimeout(gameLoop, personVis ? 1000:3000)
+    }
+    else{
+        alert("Игра окончена!")
+        alert("Твой счёт " +score);
+    }
+
+}
